b6e765e714891f27b43cef0fa9f15b9d84945f40
e.hajiabadi <E.hajiabadi@ecoapp.ir>
2024-04-24 14:55:22 +0330

    feat: implement dynamic layout for dashboard and teacher mode

--- app/(dashborad)/_components/sidebar-routes.tsx
+++ app/(dashborad)/_components/sidebar-routes.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import SidebarItem from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
   { icon: Layout, label: "Dashboard", href: "/" },
   { icon: Compass, label: "Browse", href: "/search" },
 ];
+const teacherRoutes = [
+  { icon: List, label: "Courses", href: "/teacher/courses" },
+  { icon: BarChart, label: "Analytics", href: "/teacher/analytics" },
+];
 
 const SidebarRoutes = () => {
-  const routes = guestRoutes;
-
+  const pathName = usePathname();
+  const isTeacherPage = pathName?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
   return (
     <div className=" flex flex-col w-full">
       {routes.map((route) => (

a38c59f38b6342f1fd778cde41b86b436214f16b
e.hajiabadi <E.hajiabadi@ecoapp.ir>
2024-05-01 14:43:58 +0330

    feat: create title form for update course

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,28 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { courseId } = params;
+    const values = await request.json();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const course = await db.course.update({
+      where: {
+        id: courseId,
+        userId,
+      },
+      data: { ...values },
+    });
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSE_ID]", error);
+    return new NextResponse(`internal Error `, { status: 500 });
+  }
+}

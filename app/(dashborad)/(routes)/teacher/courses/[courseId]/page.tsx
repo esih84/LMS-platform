aef99c6aeccd237b395f89418e99e7952d3a19b0
e.hajiabadi <E.hajiabadi@ecoapp.ir>
2024-07-02 20:12:39 +0330

    feat: intial  implementation of chapter form creation

--- app/(dashborad)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashborad)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -14,6 +14,7 @@ import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import AttachmentForm from "./_components/attachment-form";
+import ChaptersForm from "./_components/chapters-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -23,11 +24,17 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+      userId,
     },
     include: {
       attachments: {
         orderBy: {
           createdAt: "desc",
+        },
+      },
+      chapters: {
+        orderBy: {
+          postion: "asc",
         },
       },
     },
@@ -46,6 +53,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     course.imageUrl,
     course.price,
     course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
   ];
   const totalField = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
@@ -84,7 +92,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               <IconBadge icon={ListChecks} />
               <h2 className=" text-xl"> Course chapters</h2>
             </div>
-            <div>TODO: Chapters</div>
+            <ChaptersForm initialData={course} courseId={course.id} />
           </div>
           <div>
             <div className=" flex items-center gap-x-2">
---
+++ app/api/courses/[courseId]/chapters/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export const POST = async (
+  request: Request,
+  { params: { courseId } }: { params: { courseId: string } }
+) => {
+  try {
+    const { userId } = auth();
+    const { title } = await request.json();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId,
+      },
+    });
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const lastChapter = await db.chapter.findFirst({
+      where: {
+        courseId,
+      },
+      orderBy: {
+        postion: "desc",
+      },
+    });
+    const newPosition = lastChapter ? lastChapter.postion + 1 : 1;
+    const chapter = await db.chapter.create({
+      data: {
+        title,
+        courseId,
+        postion: newPosition,
+      },
+    });
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[CHAPTERS]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
